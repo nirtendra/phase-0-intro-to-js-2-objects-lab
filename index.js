b42877f03d194b46525f1ebcaca469b49bdd0af2
nirtendra <nirtendrayadav12@gmail.com>
2024-01-16 18:45:08 +1100

    complete object lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution in this file!
+
+let employee = {
+   
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    employee = { ...employee };
+    employee[key] = value;
+    return employee;
+  }
+
+
+ // console.log(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway'))
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+
+
+  
+  //console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway'))
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    let updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+
+
+  //  deleteFromEmployeeByKey(employee, 'name')
+  
+
+
+  
+  
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+   
+    return employee;
+  }
+
+
+  
+  // destructivelyDeleteFromEmployeeByKey(employee, 'name')
